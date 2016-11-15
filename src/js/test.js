d6f2b198295799890b92304702b9921f69e2436e
fujisawa-shin <fujisawa-shin@kayac.com>
2016-11-15 23:50:50 +0900

    さらにアップデートしたり

--- src/js/test.js
+++ src/js/test.js
@@ -4,7 +4,4 @@ describe('javascript', () => {
     it('calculate addition', () => {
         expect(3 + 2).to.be.equal(5);
     });
-    it('invalid calculate addition', () => {
-        expect(3 + 2).to.be.equal(8);
-    });
 });
